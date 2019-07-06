ad9e1b0fb9280e6d786bf38334dde0c94fbddc49
Robert Ingrum <ingrumr@amazon.com>
2019-07-06 00:03:37 -0700

    Basic key store logic.

--- docker/app.py
+++ docker/app.py
@@ -1,5 +1,10 @@
 import redis
+import sys
+
 from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
@@ -8,4 +13,23 @@ cache = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def root():
-    return "Hello world from the MyData!!!"
+    data = {}
+
+    for key in cache.keys():
+        key = key.decode("utf-8")
+        value = cache.get(key).decode("utf-8")
+
+        data[key] = value
+
+    return render_template("home.html", keys=data)
+
+
+@app.route("/add", methods=["post"])
+def add():
+    key = request.form.get('key', None)
+    value = request.form.get('value', None)
+
+    if key is not None and value is not None:
+        cache.set(key, value)
+
+    return redirect("/", code=302)
